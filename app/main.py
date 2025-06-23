5c04b7155938492245a60fac948be5141b8a70bf
Alberto Cesar <albertocesar.be@gmail.com>
2025-06-23 13:34:46 -0300

    feat: implementando multi threading

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import random
 import socket
+import threading
+
 import resp_codec
 storage = {}
 
 def process(args):
     command = args[0].lower()
-    print(args)
     match command:
         case "ping":
             return resp_codec.encode("PONG").encode()
@@ -14,7 +15,6 @@ def process(args):
                 return resp_codec.encode(" ").encode()
             return resp_codec.encode(args[1]).encode()
         case "set":
-            print(storage)
             key = args[1]
             if key =="key:__rand_int__":
                 key = random.randint(1, 20)
@@ -37,36 +37,38 @@ def process(args):
         case _:
             return resp_codec.encode(f'unknown command "{command}"', True).encode()
 
+def handle_client(connection: socket, address):
+    buffer = b""
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+
+            while buffer:
+                try:
+                    frame, start = resp_codec.decode_frame(buffer)
+                except resp_codec.IncompleteFrame:
+                    break
+                except Exception as e:
+                    connection.sendall(resp_codec.encode(str(e), error=True).encode())
+                    buffer = buffer[start:]
+                    continue
+
+                buffer = buffer[start:]
+                connection.sendall(process(frame))
+    finally:
+        print(f"Connection with {address} closed")
+        connection.close()
+
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
         connection, address = server_socket.accept()
         print(f"Client {address} connected")
         buffer = b""
-        while True:
-            data = connection.recv(4096)
-            if not data:
-                break
-
-            buffer += data
-            print(buffer)
-            try:
-                while buffer:
-                    try:
-                        frame, used = resp_codec.decode_frame(buffer)
-                    except resp_codec.IncompleteFrame:
-                        break
-                    except Exception as e:
-                        connection.sendall(resp_codec.encode(str(e), error=True).encode())
-                        buffer = buffer[used:]
-                        continue
-                    buffer = buffer[used:]
-                    reply = process(frame)
-                    connection.sendall(reply)
-            except Exception as e:
-                print("Erro no decode:", e)
-                connection.sendall(resp_codec.encode("ERR decoding", True).encode())
-                continue
+        threading.Thread(target=handle_client, args=(connection, address), daemon=True).start()
 
 if __name__ == "__main__":
     main()
